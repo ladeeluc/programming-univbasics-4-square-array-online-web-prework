b5e41589793755e982916d05061d0313680930db
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-20 18:25:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while array[counter]do
+    puts array[counter]
+    counter +=1
+  end
+def square_array(array)
+puts Math.sqrt(array)
+end
+
+end
+
+
 end
